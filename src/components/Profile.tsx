878b6deb65dbd31a748f5c36142632044fd7aecc
Matheus Felizardo <matheus.felizardo2@gmail.com>
2021-02-24 01:31:37 -0300

    alterando para next

---
+++ src/components/Profile.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from '../styles/components/Profile.module.css'
+
+export const Profile = () => {
+    return (
+        <div className={styles.profileContainer} >
+            <img src="https://github.com/MatheusFelizardo.png" alt="Matheus Felizardo"/>
+
+            <div>
+                <strong>Matheus Felizardo</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level 1
+                </p>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+//Utilizado para armezenar todo conteúdo realmente estático, que não irão mudar independente da página. Ex: link para fonte
+
+import Document, { Html, Head, Main, NextScript} from 'next/document'
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="pt-BR">  
+                <Head>
+                    <link rel="shortcut icon" href="favicon.png" type="image/png"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Rajdhani:wght@400;500;600;700&display=swap" rel="stylesheet" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
