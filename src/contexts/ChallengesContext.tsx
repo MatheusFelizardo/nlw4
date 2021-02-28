bcf46ee3ee227d2184805131073ac675203bf98b
Matheus Felizardo <matheus.felizardo2@gmail.com>
2021-02-28 20:31:50 -0300

    fix: ajuste notificaçao mobile

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -69,7 +69,10 @@ export function ChallengesProvider ({children, ...rest}:ChallengesProviderProps)
 
             if(navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i) )
             {   
-                return false
+                setTimeout(()=> {
+                    alert(`Novo desafio valendo ${challenge.amount}xp 🏆`)
+                }, 100)
+                
             }
             else {
                 new Notification('🎉 Novo desafio  🎉', {
